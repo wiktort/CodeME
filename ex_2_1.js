337cc84102a264f538b0f857fb77a1e8c52f5db0
Wiktor Tuminski <wiktor.tuminski@trip4share.com>
2020-10-30 23:18:13 +0100

    preparing an environment for homeworks

---
+++ ex_2_1.js
@@ -0,0 +1,28 @@
+/**
+ * Zadeklaruj stala CITY_SPEED_LIMIT i przypisz jej wartosc 50,
+ * zadeklaruj stala TWO_LANE_EXPRESSWAY_SPEED_LIMIT i przypisz jej wartosc 120,
+ * zadeklaruj stala HIGHWAY_SPEED_LIMIT i przypisz jej wartosc 140.
+ *
+ * Utworz zmienna speed, w ktorej bedzie mozna zmieniac wartosc.
+ * Niech wartosc poczatkowow tej zmiennej wynosi 49.
+ *
+ * Utworz zmienna typeOfRoad, w ktorej bedzie mozna zmieniac wartosc.
+ * Niech wartosc poczatkowa tej zmiennej wynosi 'city'.
+ *
+ * Napisz instrukcje warunkowa (if, switch lub jedna i druga), w której jezeli zostana spelnione warunki:
+ *
+ * 1. typeOfRoad ma wartosc 'city' i speed jest mniejsze od wartosci CITY_SPEED_LIMIT
+ * to zostanie wyswietlony tekst 'Miasto: jedziesz prawidlowo'
+ * jezeli ta predkosc jest wieksza od wartosci CITY_SPEED_LIMIT - wyswietlony zostanie tekst: 'Miasto: przekroczyles prędkość'.
+ *
+ * 2. typeOfRoad ma wartosc 'expressway' i speed jest mniejsze od wartosci TWO_LANE_EXPRESSWAY_SPEED_LIMIT
+ * to zostanie wyswietlony tekst 'Eska: jedziesz prawidlowo'
+ * jezeli ta predkosc jest wieksza od wartosci TWO_LANE_EXPRESSWAY_SPEED_LIMIT
+ * - wyswietlony zostanie tekst: 'Eska: przekroczyles prędkość'.
+ *
+ * 3. typeOfRoad ma wartosc 'highway' i speed jest mniejsze od wartosci HIGHWAY_SPEED_LIMIT
+ * to zostanie wyswietlony tekst 'Autostrada: jedziesz prawidlowo'
+ * jezeli ta predkosc jest wieksza od wartosci HIGHWAY_SPEED_LIMIT
+ * - wyswietlony zostanie tekst: 'autostrada: przekroczyles prędkość'.
+ *
+ */
---
+++ ex_2_2.js
@@ -0,0 +1,14 @@
+/**
+ * Zmien tak wytyczne zadania [ex_2_1], zeby napisac funkcje,
+ * ktora bedzie przyjmowala dwa parametry: speed
+ * i typeOfRoad. Sprobuj ustawić wartosc typeOfRoad na 'city'
+ * funkcja powinna wypisywac to co jest zapisane w zadaniu [ex_2_1]
+ * kiedy przy wywolaniu podajemy speed i/lub typeOfRoad.
+ *
+ * Czyli jezeli wywolam funkcje:
+ *
+ * myFunction(30); to zostanie wyswietlony tekst 'Miasto: jedziesz prawidlowo'
+ * myFunction(150, 'expressway'); zostanie wyswietlony tekst 'Eska: przekroczyles prędkość'
+ * itd.
+ *
+ */
